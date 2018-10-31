50c8f03f98122192f2d4917f7fd6372d4679aaa4
tylerscheuble <tyler@scheuble.us>
2018-10-30 17:46:01 -0700

    Add base files for workshop

---
+++ src/main/kotlin/org/team2471/workshop/Robot.kt
@@ -0,0 +1,20 @@
+package org.team2471.workshop
+
+import edu.wpi.first.wpilibj.TimedRobot
+
+class Robot : TimedRobot() {
+    // This function is run once when the robot boots up.
+    override fun robotInit() {
+        TODO()
+    }
+
+    // This function is run once every time the robot is enabled in teleop mode.
+    override fun teleopInit() {
+        TODO()
+    }
+
+    // This function is run 50 times per second while the robot is enabled in teleop mode.
+    override fun teleopPeriodic() {
+        TODO()
+    }
+}
